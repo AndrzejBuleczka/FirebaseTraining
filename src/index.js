7b81ab868f5811c5933e00012fd9f560044021b4
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2022-10-26 20:49:19 +0200

    adding firebase to the project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('hello from index.js');
+import {initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCgwYIsOD5adBvIDsLd5Qq4CYDK1R1X2es",
+  authDomain: "fir-9-schola.firebaseapp.com",
+  projectId: "fir-9-schola",
+  storageBucket: "fir-9-schola.appspot.com",
+  messagingSenderId: "474268649630",
+  appId: "1:474268649630:web:e442cb5bfb333b96e1138f"
+};
+
+initializeApp(firebaseConfig)
